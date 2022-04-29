97a10f928717e51d752f7c2c07908892a0819c23
Lorenzo Greco <lorenzo.rev@gmail.com>
2022-04-29 10:56:38 +0200

    add collector config

--- example/start.sh
+++ example/start.sh
@@ -1,15 +1,18 @@
 read -p 'Insert your GitLab Token: ' gitlabToken
 
 git clone -b develop https://github.com/odemolliens/adash-data-collector.git adash-data-collector-example
+cp config-collector.json adash-data-collector-example/config.json
 cd adash-data-collector-example
+git pull
 yarn
 
-sed -i '' "s/REDACTED/$gitlabToken/g" example/config.json
+sed -i '' "s/REDACTED/$gitlabToken/g" config.json
 cd ..
 git clone -b develop https://github.com/odemolliens/adash-rn-web.git adash-rn-web-example
 
 mkdir adash-rn-web-example/data
-sh update-data.sh
-cp config.json adash-rn-web-example/
+sh update.sh
+cp config-dashboard.json adash-rn-web-example/config.json
 cd adash-rn-web-example
+git pull
 yarn && yarn dev
--- example/update-data.sh
+++
@@ -1,8 +0,0 @@
-cd adash-data-collector-example
-
-DEBUG=1 npx ts-node adash-data-collector collect --config example/config.json
-DEBUG=1 npx ts-node adash-data-collector notificator --config example/config.json
-
-cd ..
-
-cp -Rf adash-data-collector-example/data/* adash-rn-web-example/data
